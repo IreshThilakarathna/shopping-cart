8a41101087e43d8ff5abc268a516a444c62e8c8d
IreshThilakarathna <thilakarathnairesh@gmail.com>
2020-07-11 18:22:14 +0530

    add redux to filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
@@ -14,10 +13,7 @@ class App extends React.Component {
       items = JSON.parse(localStorage.getItem("cartItems"));
     }
     this.state = {
-      products: data.products,
       cartItems: items,
-      size: "",
-      sort: "",
     };
   }
 
@@ -51,44 +47,6 @@ class App extends React.Component {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
-  sortProducts = (event) => {
-    console.log(event.target.value);
-    const sort = event.target.value;
-
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    }));
-  };
-
-  filterProducts = (event) => {
-    console.log(event.target.value);
-    if (event.target.value === "") {
-      this.setState({ size: event.target.value, products: data.products });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  };
-
   render() {
     return (
       <Provider store={store}>
@@ -99,17 +57,8 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter
-                  count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                ></Filter>
-                <Products
-                  products={this.state.products}
-                  addToCart={this.addToCart}
-                ></Products>
+                <Filter></Filter>
+                <Products addToCart={this.addToCart}></Products>
               </div>
               <div className="sidebar">
                 <Cart
--- src/reducers/productsReducers.js
+++ src/reducers/productsReducers.js
@@ -1,9 +1,25 @@
-const { FETCH_PRODUCTS } = require("../types");
+const {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} = require("../types");
 
 export const productsReducer = (state = {}, action) => {
   switch (action.type) {
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        size: action.payload.size,
+        filteredItems: action.payload.items,
+      };
+    case ORDER_PRODUCTS_BY_PRICE:
+      return {
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
     case FETCH_PRODUCTS:
-      return { items: action.payload };
+      return { items: action.payload, filteredItems: action.payload };
     default:
       return state;
   }
